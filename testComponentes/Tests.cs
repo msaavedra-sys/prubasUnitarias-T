b84ed4ce2fa213f16f0849d4bdd0d7e2d90cf8bd
miguel <msaavedra@kubo.co>
2025-11-06 21:18:07 -0500

    pruebas

---
+++ testComponentes/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Metrics;
+using testComponentes.Data;
+using Bunit;
+using BlazorApp1.Components.Pages;
+
+namespace testComponentes;
+
+public class Tests
+{
+    [Test]
+    public async Task Basic()
+    {
+        Console.WriteLine("This is a basic test");
+        var bunitctx = new Bunit.TestContext();
+        var componente = bunitctx.RenderComponent<Counter>();
+        componente.Find("button").Click();
+        var resultado = componente.Find("p[role='status']");
+        await TUnit.Assertions.Assert.That(resultado.TextContent).Contains("Current count: 1");
+    }
+
+   
+
+
+
+}
